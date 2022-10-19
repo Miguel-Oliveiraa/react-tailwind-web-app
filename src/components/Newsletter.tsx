12f07e172420f2979e98c796dac6df41111a0e66
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-10-19 19:57:45 -0300

    :sparkles: feat: newsletter component

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -2,9 +2,30 @@ import React from "react";
 
 export function Newsletter() {
   return (
-    <div>
-      <div>
-        <p>oi</p>
+    <div className="w-full py-16 text-white px-4">
+      <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
+        <div className="lg:col-span-2 my-4">
+          <h1 className="text-2xl font-bold py-2 sm:text-3xl md:text-4xl">
+            Want tips & tricks to optimize your flow?
+          </h1>
+          <p className="">Sign up to our newsletter and stay up to date.</p>
+        </div>
+        <div className="my-4">
+          <div className="flex flex-col sm:flex-row items-center justify-between">
+            <input
+              className="p-3 flex w-full rounded-sm text-black"
+              type="email"
+              placeholder="Enter Email"
+            />
+            <button className="bg-[#00df9a] w-[200px] rounded-sm font-medium ml-4 my-6 px-6 py-3 text-black">
+              Notify Me
+            </button>
+          </div>
+          <p>
+            We care about the protection of your data. Read our{" "}
+            <span className="text-[#00df9a]">Privacy Policy.</span>
+          </p>
+        </div>
       </div>
     </div>
   );
