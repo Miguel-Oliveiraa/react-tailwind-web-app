b7fe2899bca523dea7e64475ce7e5427f94721bc
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-10-19 20:09:59 -0300

    :sparkles: feat: cards component

---
+++ src/components/Card.tsx
@@ -0,0 +1,7 @@
+export function Cards() {
+  return (
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240] mx-auto grid grid-cols-3 gap-8"></div>
+    </div>
+  );
+}
