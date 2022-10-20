bd39c62be9e69383ab1bed4bd91198705c74c7ba
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-10-20 14:41:20 -0300

    :sparkles: feat: footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Cards } from "./components/Card";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 import { Newsletter } from "./components/Newsletter";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Analytics />
       <Newsletter />
       <Cards />
+      <Footer />
     </div>
   );
 }
